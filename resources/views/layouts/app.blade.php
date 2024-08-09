1bd505a2afd714dbc143e6a7b4b30f143475274e
Titus Patrick <tituspat0112@gmail.com>
2024-08-09 19:27:31 +0700

    fix: login stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -32,5 +35,8 @@
                 {{ $slot }}
             </main>
         </div>
+
+
+        <script src="{{ asset('js/custom.js') }}"></script>
     </body>
 </html>
