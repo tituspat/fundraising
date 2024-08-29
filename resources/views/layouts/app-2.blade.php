4b2ff6e2ccbb9e42a82dd57d5728ddacdc75b19d
Titus Patrick <tituspat0112@gmail.com>
2024-08-29 21:26:00 +0700

    feat: block login and layout dashboard

--- resources/views/layouts/app-2.blade.php
+++ resources/views/layouts/app-2.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Turut berkontribusi mewujudkan Tabanan yang sejahtera bersama SAHABAT Tabanan.">
+    <meta property="og:title" content="I Gede Putu Atma Giri | Sahabat Tabanan">
+    <meta property="og:description" content="Turut berkontribusi mewujudkan Tabanan yang sejahtera bersama SAHABAT Tabanan.">
     <title>Sahabat Tabanan</title>
     <!-- Favicon -->
     <link rel="icon" type="icon" href="{{asset('img/logo-sahabat-tabanan.png')}}" />
