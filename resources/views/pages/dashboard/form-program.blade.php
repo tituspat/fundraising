a0285d4d82fdb5b3eb0eff7f79582e5db8d6de65
inggritrs <inggritrismauli26@gmail.com>
2024-08-31 18:21:50 +0700

    form-program

--- resources/views/pages/dashboard/form-program.blade.php
+++ resources/views/pages/dashboard/form-program.blade.php
@@ -28,7 +28,7 @@
                               	<!-- Middle (Conditional) -->
 
                               		<li class="inline-flex items-center before:content-[''] before:w-1 before:h-1 before:ltr:float-left rtl:float-right before:bg-light-extra before:me-[7px] before:pe-0 before:rounded-[50%]">
-                              	   		<span class="text-[14px] font-normal leading-[20px] text-body dark:text-neutral-200 transition duration-300 capitalize">features</span>
+                              	   		<span class="text-[14px] font-normal leading-[20px] text-body dark:text-neutral-200 transition duration-300 capitalize">Programs</span>
                               		</li>
 
                               	<!-- Child (Current Page) -->
@@ -62,25 +62,23 @@
                                 </label>
                             </div>
 
-                            <!-- Image Preview -->
-                            <div id="image-preview" class="mt-4 hidden flex items-center border rounded-md px-4 py-2 bg-gray-50">
-                                <!-- Image Thumbnail -->
-                                <img id="preview" src="" alt="Image Preview" class="w-8 h-8 object-cover rounded-md mr-4">
-                                <!-- File Information -->
-                                <div class="flex-1">
-                                    <p id="file-name" class="text-sm font-medium text-gray-700 truncate overflow-hidden"></p>
-                                    <p id="file-size" class="text-xs text-gray-500"></p>
+                                <!-- Image Preview -->
+                                <div id="image-preview" class="mt-4 hidden flex items-center border rounded-md px-4 py-2 bg-gray-50">
+                                    <!-- Image Thumbnail -->
+                                    <img id="preview" src="" alt="Image Preview" class="w-8 h-8 object-cover rounded-md mr-4">
+                                    <!-- File Information -->
+                                    <div class="flex-1">
+                                        <p id="file-name" class="text-sm font-medium text-gray-700 truncate overflow-hidden"></p>
+                                        <p id="file-size" class="text-xs text-gray-500"></p>
+                                    </div>
+                                    <!-- Remove Button -->
+                                    <button type="button" onclick="removePreview()" class="text-gray-400 hover:text-gray-600">
+                                        &times;
+                                    </button>
                                 </div>
-                                <!-- Remove Button -->
-                                <button type="button" onclick="removePreview()" class="text-gray-400 hover:text-gray-600">
-                                    &times;
-                                </button>
                             </div>
-                        </div>
-                        
                         <input type="hidden" name="created_by" value="{{ Auth::user()->name }}">
-						
-                        <div class="my-4">
+						<div class="my-4">
 							<label for="titleVertical" class="block text-sm font-medium capitalize text-dark dark:text-title-dark">Judul Program</label>
 							<input type="text" id="nameVertical" class="rounded-4 border-normal border-1 text-[15px] dark:bg-box-dark-up dark:border-box-dark-up px-[20px] py-[12px] min-h-[50px] outline-none placeholder:text-[#A0A0A0] text-body dark:text-subtitle-dark w-full focus:ring-primary focus:border-primary" name="title" placeholder="Judul Program" required>
                         </div>
@@ -111,38 +109,45 @@
 
             @elseif($isEditing)
 			<form action="{{ route(Auth::user()->role . '.program.update', $program->id) }}" method="POST" enctype="multipart/form-data">
-			@csrf
-			<div class="col-span-12 md:col-span-4">
-                <div class="bg-white dark:bg-box-dark m-0 p-0 text-body dark:text-subtitle-dark text-[15px] rounded-10 relative">
-                    <div class="p-[25px]">
+                @csrf
+                <div class="col-span-12 md:col-span-4">
+                    <div class="bg-white dark:bg-box-dark m-0 p-0 text-body dark:text-subtitle-dark text-[15px] rounded-10 relative">
+                        <div class="p-[25px]">
 
-                    <div class="p-[25px] mb-4">
-                            <div class="flex items-center justify-center w-full">
+                            <!-- Preview Thumbnail Existing -->
+                            @if($program->thumbnail)
+                            <div class="mb-4">
+                                <p class="text-sm font-medium capitalize text-dark dark:text-title-dark">Thumbnail Saat Ini</p>
+                                <center><img src="{{ asset($program->thumbnail) }}" alt="Thumbnail Saat Ini" class="w-[150px] h-[150px] object-cover rounded-md mb-4"></center>
+                                <p class="text-sm text-gray-500">Ubah Thumbnail</p>
+                            </div>
+                            @endif
+
+                            <!-- Input Thumbnail -->
+                            <div class="mb-4">
                                 <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-[176px] transition-all duration-300 ease-linear bg-white border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100 dark:bg-box-dark-up dark:border-box-dark-up dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                     <div class="flex flex-col items-center justify-center py-5">
                                         <p class="text-[15px] text-light font-medium dark:text-subtitle-dark">
                                             Drop files here to upload
                                         </p>
                                     </div>
-                                    <input id="dropzone-file" type="file" name="image" class="hidden" value="{{ $program->thumbnail }}" accept="image/*" onchange="previewImage(event)" required />
+                                    <input id="dropzone-file" type="file" name="image" class="hidden" accept="image/*" onchange="previewImage(event)" />
                                 </label>
-                            </div>
-
-                            <!-- Image Preview -->
-                            <div id="image-preview" class="mt-4 hidden flex items-center border rounded-md px-4 py-2 bg-gray-50">
-                                <!-- Image Thumbnail -->
-                                <img id="preview" src="" alt="Image Preview" class="w-8 h-8 object-cover rounded-md mr-4">
-                                <!-- File Information -->
-                                <div class="flex-1">
-                                    <p id="file-name" class="text-sm font-medium text-gray-700 truncate overflow-hidden"></p>
-                                    <p id="file-size" class="text-xs text-gray-500"></p>
+                                <!-- Image Preview -->
+                                <div id="image-preview" class="mt-4 hidden flex items-center border rounded-md px-4 py-2 bg-gray-50">
+                                    <!-- Image Thumbnail -->
+                                    <img id="preview" src="" alt="Image Preview" class="w-8 h-8 object-cover rounded-md mr-4">
+                                    <!-- File Information -->
+                                    <div class="flex-1">
+                                        <p id="file-name" class="text-sm font-medium text-gray-700 truncate overflow-hidden"></p>
+                                        <p id="file-size" class="text-xs text-gray-500"></p>
+                                    </div>
+                                    <!-- Remove Button -->
+                                    <button type="button" onclick="removePreview()" class="text-gray-400 hover:text-gray-600">
+                                        &times;
+                                    </button>
                                 </div>
-                                <!-- Remove Button -->
-                                <button type="button" onclick="removePreview()" class="text-gray-400 hover:text-gray-600">
-                                    &times;
-                                </button>
                             </div>
-                        </div>
 
                         <input type="hidden" name="updated_by" value="{{ Auth::user()->name }}">
 
